7c1f7a488b0bd8ebed2c2f1a20fb2b98a36ca132
ashmi721 <ashmitak721@gmail.com>
2023-10-06 22:30:38 +0545

    design template file also create the url

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from users.views import home
+from users.views import home,user_login,user_register
 
 urlpatterns = [
     path('',home,name = "home_page"),
+    path('login/',user_login,name = "login_page"),
+    path('register/',user_register,name = "register_page"),
+    
 ]
