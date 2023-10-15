c4ea45dfb4e0714895ced277868bf88778a67ad4
ashmi721 <ashmitak721@gmail.com>
2023-10-15 23:36:01 +0545

    complete to make the product_detail and cart page

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import shopping_cart,checkout,order_summary,add_to_cart
+from .views import shopping_cart,checkout,order_summary,add_to_cart,remove_cart
 
 urlpatterns = [
     path('cart/',shopping_cart,name = "cart_page"),
     path('checkout/',checkout,name = "checkout_page"),
     path('order-summary/',order_summary,name = "order_summary_page"),
-    path("add-to-cart/<int:product_id>",add_to_cart,name="add_to_cart")
+    path("add-to-cart/<int:product_id>",add_to_cart,name="add_to_cart_page"),
+    path("remove-cart/<int:cart_id>",remove_cart,name="remove_cart_page"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -5,8 +5,18 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def shopping_cart(request):
-    carts = Cart.objects.filter(user=request.user).order_by("-created_at")
-    return render(request,"orders/cart.html",{"carts":carts})
+    carts = Cart.objects.filter(user=request.user).order_by("created_at")
+    has_items = len(carts) >0
+    total_cart_amount =0
+    grand_total= 0
+    vat_amount = 0
+    if has_items:
+        # total_cart_amount = sum([Cart.cart_total for cart in carts])
+        for item in carts:
+            total_cart_amount += item.cart_total
+        vat_amount = total_cart_amount * 0.13
+        grand_total = total_cart_amount + vat_amount
+    return render(request,"orders/cart.html",{"carts":carts,"has_items":has_items,"total_cart_amount":total_cart_amount,"vat_amount":vat_amount,"grand_total":grand_total})
 
 @login_required
 def add_to_cart(request,product_id):
@@ -18,6 +28,11 @@ def add_to_cart(request,product_id):
         "cart_total":product.price,
     }
     Cart.objects.create(**cart_data)
+    return redirect("/cart")
+
+def remove_cart(request,cart_id):
+    cart = Cart.objects.get(pk=cart_id)
+    cart.delete()
     return redirect("/cart")
 
 def checkout(request):
